86ca2ebdefba01a0330ce9be5cfcf270089931c6
bharath-02 <bharathj222000@gmail.com>
2026-09-01 21:36:40 +0530

    add port env

--- Backend/app.js
+++ Backend/app.js
@@ -59,7 +59,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB Connection Successful!!!");
-    app.listen(5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log("Server listening on port 5000");
     });
   })
